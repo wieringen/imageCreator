db5bcacc32afc750b31316b26393a13748def5f5
Maarten Baijs <maarten.baijs@marviq.com>
2013-04-18 16:27:51 +0200

    UI Library: Forgot to commit some files for the UI library module.

---
+++ src/core/ui.library.js
@@ -0,0 +1,179 @@
+/**
+ * Image tool Class
+ *
+ * @name Image
+ * @class Image
+ * @constructor
+ *
+ */
+define(
+[
+    // Module HTML template.
+    //
+    "text!templates/library.html"
+
+    // App core modules.
+    //
+,   "config"
+,   "cache"
+,   "model.image"
+
+    // jQuery plugins.
+    //
+,   "plugins/jquery.tabular"
+,   "plugins/jquery.dropArea"
+],
+function( moduleHTML, config, cache, modelImage )
+{
+    var module =
+        {
+            name     : "library"
+        ,   enabled  : true
+        ,   options  : config.options.ui.library
+        ,   snippets : {}
+        }
+
+    ,   $imageCreatorViewport
+
+    ,   $module
+    ,   $moduleTitle
+    ,   $libraryUploadSubmit
+    ,   $libraryUploadFrame
+    ,   $libraryUploadForm
+
+    ,   layerCurrent = false
+    ;
+
+
+    /**
+      * Function that initializes the module. It will append the modules html, set the title and initializes its UI.
+      *
+      * @name Image#initialize
+      * @function
+      *
+      */
+    module.initialize = function( options )
+    {
+        // Append module HTML.
+        //
+        $( module.options.target ).replaceWith( moduleHTML );
+
+        // Get basic app DOM elements.
+        //
+        $imageCreatorViewport  = $( ".imageCreatorViewport" );
+        $imageCreatorToolbar   = $( ".imageCreatorToolbar" );
+
+        // Get module DOM elements.
+        //
+        $module              = $( module.options.target );
+        $moduleTitle         = $module.find( ".moduleTitle" );
+        $libraryUploadSubmit = $module.find( ".libraryUploadSubmit" );
+        $libraryUploadFrame  = $module.find( ".libraryUploadFrame" );
+        $libraryUploadForm   = $module.find( ".libraryUploadForm" );
+
+        // Set module title.
+        //
+        $moduleTitle.text( module.options.title );
+
+        // Initialize module UI.
+        //
+        $module.tabular(
+        {
+            "menu"     : ".moduleMenu"
+        ,   "tabs"     : "a"
+        ,   "pages"    : ".moduleTab"
+        });
+        $imageCreatorViewport.dropArea();
+
+        // Listen to global app events.
+        //
+        $.subscribe( "layerSelect", layerSelect );
+        $.subscribe( "layerVisibility", layerSelect );
+        $.subscribe( "fileUpload", imageAdd );
+
+        // Set Button events.
+        //
+        $libraryUploadSubmit.bind( "click", imageUpload );
+        $buttonImageAdd.click( function()
+        {
+            cache.setLayerActive( false );
+
+            $module.removeClass( "moduleDisabled" );
+
+            return false;
+        });
+
+    };
+
+    function layerSelect( event, layer )
+    {
+        $module.addClass( "moduleDisabled" );
+    }
+
+    function imageUpload()
+    {
+        // Temp!!! Just used for debugging puposes.
+        //
+        $libraryUploadForm.submit();
+        $libraryUploadFrame.unbind( "load" ).load( function( event )
+        {
+            var json = $.parseJSON( $( this ).contents().text() );
+
+            if( json && json.code !== 200 )
+            {
+                $imageCreatorViewport.trigger( "setMessage", [ {
+                    "message" : json.message
+                ,   "status"  : "error"
+                ,   "fade"    : false
+                }]);
+            }
+            else
+            {
+                imageAdd( json.src );
+            }
+        });
+
+        return false;
+    }
+
+    function backgroundAdd( url )
+    {
+        var layer =
+        {
+            locked    : true
+        ,   plane     : "background"
+        ,   position  :
+            {
+                x : 0
+            ,   y : 0
+            }
+        ,   scale : 1
+        ,   src : typeof url === "string" ? url : this.src
+        };
+
+        modelImage.fromObject( layer, function( instance )
+        {
+            cache.setLayerActive( instance );
+        });
+
+        return false;
+    }
+
+    function imageAdd( url )
+    {
+        var layer =
+        {
+            src   : typeof url === "string" ? url : this.src
+        ,   scale : 0.3335
+        };
+
+        modelImage.fromObject( layer, function( instance )
+        {
+            cache.setLayerActive( instance );
+        });
+
+        return false;
+    }
+
+    return module;
+} );
